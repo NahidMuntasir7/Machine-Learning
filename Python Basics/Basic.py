48ff6f0093e4053bdf2d42a7c74e205290888be2
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-02-24 20:21:11 +0600

    Create Basic.py

---
+++ Python Basics/Basic.py
@@ -0,0 +1,32 @@
+text = "Hello sire!"
+print(text)
+
+num1 = 2
+num2 = 5
+res = num1 + num2
+print(res)
+print(num1 + num2)
+
+s = 'abc 123'
+print(s)
+
+print(5 / 2)
+print(5 // 2) # int part of the ans
+print(5 % 2)
+print(5 ** 2) # square
+
+a = 1
+b = 1.2
+c = "hello"
+print(a, b, c)
+print(a, b, c, "world")
+
+#input
+name = input("What is your name?")
+print("Welcome to Python,", name, "!")
+
+num1 = input("Enter the first num :")
+num2 = input("Enter the second num :")
+num1 = int(num1) # a + b = ab if no typecast
+num2 = int(num2)
+print("Number1 + Number2 = ", num1 + num2)
