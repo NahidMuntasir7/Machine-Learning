77ede52e82dc9c97ccc3469462a582102f39fcf5
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-02-24 23:34:13 +0600

    Update 3. Loop.py

--- Python Basics/3. Loop.py
+++ Python Basics/3. Loop.py
@@ -66,4 +66,56 @@ print("Sum is : ", result)
 
 
 # nested loop
+for i in range(1, 5):
+    for j in range(4):
+        print(i)
 
+
+# looping in list : 
+saarc = ["BD", "IND", "PAK", "AFG"]
+
+for country in saarc:
+    print(country, "is a member of SAARC")
+  
+    
+# making list using range
+li = list(range(11)) # 0 to 10
+print(li)
+
+li = list(range(2, 21, 2)) # 2, 4, 6 ... ,20
+print(li)
+
+# while loop :
+i = 0
+while i < 5:
+    print(i)
+    i += 1
+    
+i = 5
+while i >= 0:
+    i -= 1
+    print(i)
+    
+# namta using while loop :
+
+n = input("Enter a n : ")
+n = int(n)
+
+m = 1
+while (m <= 10):
+    print(n, "X", m, "=", n * m)
+    m += 1
+
+# for loop inside while loop :
+
+cnt = 1
+while(cnt <= 5):
+    for i in range(3):
+        print(i)
+    print("--------")
+    cnt += 1
+    
+# break and continue :
+
+while true:
+    n = input("Enter any number ()")
