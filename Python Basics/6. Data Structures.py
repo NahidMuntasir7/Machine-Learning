97fe792b8a672ff448907198130812fcf9f96f6d
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-02-25 21:39:08 +0600

    Update 6. Data Structures.py

--- Python Basics/6. Data Structures.py
+++ Python Basics/6. Data Structures.py
@@ -131,8 +131,22 @@ for x in li:
 print(newli) # [2, 4, 6, 8]
 
 # using list comp.
+
 newli = [2 * x for x in li]
 print(newli) # [2, 4, 6, 8]
 
+
 # example 2
 li = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
+
+evennumbers = []
+for x in li:
+    if x % 2 == 0:
+        evennumbers.append(x)
+
+print(evennumbers)
+
+# using list comp.
+
+evennumbers = [x for x in range(1, 11) if x % 2 == 0]
+print(evennumbers)
