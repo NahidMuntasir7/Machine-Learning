f7c61ab9da3b3e698a5925daf425e44a28344c14
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-02-25 22:26:35 +0600

    Update 6. Data Structures - Tuple.py

--- Python Basics/6. Data Structures - Tuple.py
+++ Python Basics/6. Data Structures - Tuple.py
@@ -28,4 +28,33 @@ tpl = (1, 2, 3)
 # tpl[0] = 7  will throw errorrrrrr cause not mutable
 
 
-#
+# unpacking a tuple in some variables and then pack them again in tuple
+
+numbers = (10, 20, 30, 40)
+n1, n2, n3, n4 = numbers  # unpacking
+
+print(n1) # 10
+print(n3) # 30
+
+t = n3, n4  # packing
+print(t)
+
+
+# we can store various things in tuple and loop on them
+
+items = (1, 2, 5.5, ["a", "b", "c"], ("apple", "mango"))
+for item in items:
+    print(item, type(item))
+
+# accessing any particualar value     
+print(items[3])
+print(items[3][0])
+print(items[4][1])
+print(items[4])
+
+
+# tuple to list
+
+tpl = (1, 2, 3)
+li = list(tpl)
+print(li)
