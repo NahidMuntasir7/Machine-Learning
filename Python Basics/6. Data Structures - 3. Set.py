7df0ef97e541552e4dca26b4e518122ad4cd3e79
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-02-26 10:11:33 +0600

    Update 6. Data Structures - 3. Set.py

--- Python Basics/6. Data Structures - 3. Set.py
+++ Python Basics/6. Data Structures - 3. Set.py
@@ -1,1 +1,67 @@
+# collection of items
 
+# empty set
+
+a = set()
+print(type(a)) # <class 'set'>
+
+
+# set from given elements
+
+items = {"pen", "laptop", "cellphone", "pen"}
+print(items) # all elements are distinct & in sorted order
+# {'laptop', 'pen', 'cellphone'}
+
+
+# adding items
+
+sett = {"apple"}
+sett.add("banana")
+print(sett) # {'banana', 'apple'}
+
+a = set() # empty set
+a.add("mango")
+print(a) # {'mango'}
+
+b = set()
+b.add("orange")
+print(b) # {'orange'}
+
+
+# set from a List or a Tuple or a String
+
+li = [1, 2, 3, 4]
+tpl = (1, 2, 3)
+string = "Bangla desh"
+A = set(li)
+print(A) # {1, 2, 3, 4}
+
+B = set(tpl)
+print(B) # {1, 2, 3}
+
+C = set(string) 
+print(C) # {' ', 'l', 'e', 's', 'd', 'B', 'a', 'n', 'g', 'h'}
+
+
+# checking an element belongs to a set or not
+
+A = {1, 2, 3}
+print(1 in A) # True
+
+
+# Some set operations
+
+A = {1, 2, 3, 4, 5}
+B = {2, 4, 6, 8}
+
+C = A & B # common
+print(C)  # {2, 4}
+C = A | B # all
+print(C)  # {1, 2, 3, 4, 5, 6, 8}
+C = A ^ B # not common
+print(C) # {1, 3, 5, 6, 8}
+
+C = A - B
+print(C) # {1, 3, 5}
+C = B - A
+print(C) # {8, 6}
