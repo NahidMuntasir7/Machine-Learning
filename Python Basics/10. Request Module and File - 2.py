93b148480e28e6e6843d9c71624381d57dae3b6a
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-02-29 20:27:41 +0600

    Update 10. Request Module and File - 2.py

--- Python Basics/10. Request Module and File - 2.py
+++ Python Basics/10. Request Module and File - 2.py
@@ -1,1 +1,12 @@
+# downloading photo from internet
+
+import requests
+
+img_url = "https://goo.gl/JxktPw"
+r = requests.get(img_url)
+
+with open("pybook.png", "wb") as f: # wb = write binary
+    f.write(r.content) # r.text won't work; r.content = byte class er object
+
+
 
