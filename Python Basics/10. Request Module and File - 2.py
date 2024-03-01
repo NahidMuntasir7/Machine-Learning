d67ad112f6924986d307222566ee0ca4a71d01f9
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-03-01 16:24:35 +0600

    Update 10. Request Module and File - 2.py

--- Python Basics/10. Request Module and File - 2.py
+++ Python Basics/10. Request Module and File - 2.py
@@ -15,3 +15,40 @@ with open("pybook.png", "wb") as f: # wb = write binary
 # command line thekei jeno chobir link ar filename deya jaay
 # so that, baar baar chobir link ar naam change na kora laage
 
+import sys
+import requests
+
+# example
+a = 10
+b = 5
+print(a + b)
+
+# to convert it into command line argument
+
+print(sys.argv) # ['hello.py', '3', '6']
+print(type(sys.argv)) # <class 'list'>
+
+
+# input in terminal : python hello.py 3 6
+arguments = sys.argv
+a = int(arguments[1])
+b = int(arguments[2])
+
+print(a + b) # 9
+
+
+# now converting photo download into command line argument
+
+# import sys
+# import requests
+
+img_url = sys.argv[1]
+file_name = sys.argv[2]
+r = requests.get(img_url)
+
+with open(file_name, "wb") as f:
+    f.write(r.content)
+
+# command line: python hello.py http://goo.gl/Q7LmXw career.png
+
+# this will download a photo from that link and name that file career.png
