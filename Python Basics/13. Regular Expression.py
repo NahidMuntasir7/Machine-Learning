7b420ca3561054ab97bf9617c30b6031f38ac799
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-03-03 16:14:32 +0600

    Update 13. Regular Expression.py

--- Python Basics/13. Regular Expression.py
+++ Python Basics/13. Regular Expression.py
@@ -17,4 +17,22 @@ print(li) # [' England', ' Greenland', ' Iceland', ' Netherlands', ' New Zealand
 
 
 
-#
+import re # importing regular expression
+
+s = "Afganistan, America, Bangladesh, Canada, Denmark, England, Greenland, Iceland, Netherlands, New Zealand, Sewden, Switzerland"
+
+li = re.findall(r'(\w+lands*)', s) # ['England', 'Greenland', 'Iceland', 'Netherlands', 'Zealand', 'Switzerland']
+print(li)
+
+# Zealand
+
+
+
+# search() function to check a string inside a string or not?
+
+# import re
+
+match = re.search('Bangla', 'Bangladesh')
+print(match)  # <re.Match object; span=(0, 6), match='Bangla'>
+
+print(match.group())  # Bangla
