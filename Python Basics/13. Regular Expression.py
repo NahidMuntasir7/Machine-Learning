dbbf95e2a1c3c0e820633a7cfb746ceb1a1105a3
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-03-03 15:41:57 +0600

    Update 13. Regular Expression.py

--- Python Basics/13. Regular Expression.py
+++ Python Basics/13. Regular Expression.py
@@ -1,1 +1,20 @@
+# we need to find out the strings that ends with "land"
 
+s = "Afganistan, America, Bangladesh, Canada, Denmark, England, Greenland, Iceland, Netherlands, New Zealand, Sewden, Switzerland"
+
+countries = s.split(",")
+print(countries) # ['Afganistan', ' America', ' Bangladesh', ' Canada', ' Denmark', ' England', ' Greenland', ' Iceland', ' Netherlands', ' New Zealand', ' Sewden', ' Switzerland']
+
+li = [item for item in countries if item.endswith("land")]
+print(li) # [' England', ' Greenland', ' Iceland', ' New Zealand', ' Switzerland']
+
+
+
+# to include Netherlands
+
+li = [item for item in countries if item.endswith("land") or item.endswith("lands")]
+print(li) # [' England', ' Greenland', ' Iceland', ' Netherlands', ' New Zealand', ' Switzerland']
+
+
+
+#
