c950fb08381c8f2b759f27cd56c5119d85a60b66
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-03-19 20:44:35 +0600

    Update 4. KNN.py

--- Edureka/4. KNN.py
+++ Edureka/4. KNN.py
@@ -1,4 +1,4 @@
-
+# KNN: used in  for simple recommendation systems, pattern recognition, data mining, financial market predictions, intrusion detection, and more.
 import csv
 import random
 
@@ -44,4 +44,55 @@ def getNeighbors(trainingSet, testTnstance, k):
   length = len(testInstances) - 1
 
   for x in range(len(trainingSet)):
-    dist = euclideanDistance(
+    dist = euclideanDistance(testInstance, trainingSet[x], length)
+    distances.append(trainingSet[x], dist)
+
+  distances.sort(key=operator.itemgetter(1))
+  neighbors = []
+  for x in range(k):
+    neighbors.append(distances[x][0])
+  
+  return neighbors
+
+# step 4: get response: generate a response from a set of data instance
+
+import operator
+def getResponse(neighbors):
+  classVotes = {} 
+  for x in range(len(neighbors)):
+    response = neighbors[x][-1]
+    if response in classVotes:
+      classVotes[response] += 1
+    else:
+      classVotes[response] = 1
+  sortedVotes = sorted(classvotes.iteritems(), key=operator.itemgetter(1), reverse = True)
+  return sortedVotes[0][0]
+
+
+# step 5: accuracy
+def getAccuracy(testSet, predictions):
+  correct = 0
+  for x in range(len(testSet)):
+    if testSet[x][-1] is predictions[x]:
+      correct += 1
+  return (correct / float(len(testSet))) * 100.0
+
+
+# main
+
+def main():
+  trainingSet = []
+  testSet = []
+  split = 0.67
+  loadDataset('iris.data', split, trainingSet, testSet)
+  prediction = []
+  k = 3
+  for x in range(len(testSet)):
+    neighbors = getNeighbors(trainingSet, testSet[x], k)
+    result = getResponse(neighbors)
+    predictions.append(result)
+    print('> predicted=' + repr(result) + ', actual=' + repr(testSet[x][-1]))
+  accuracy = getAccuracy(testSet, predictons)
+  print('Accuracy: ' + repr(accuracy) + '%')
+
+main()
