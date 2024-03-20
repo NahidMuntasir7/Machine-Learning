a2f3754e5db007af71d4e6381abe0b88de81bebf
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-03-20 12:14:37 +0600

    Create 5. Naive Bayes.py

---
+++ Edureka/5. Naive Bayes.py
@@ -0,0 +1,24 @@
+# Naive bayes
+
+# implementation from scratch:
+# ..... :\
+
+
+# implementation by sklearn
+
+from sklearn import datasets
+from sklearn import metrics
+from sklearn.naive_bayes import GaussianNB
+
+dataset = datasets.load_iris()
+
+model = GaussianNB()
+model.fit(dataset.data, dataset.target)
+
+print(model)
+
+expected = dataset.target
+predicted = model.predict(dataset.data)
+
+print(metrics.classification_report(expected, predicted))
+print(metrics.confusion_matrix(expected, predicted))
